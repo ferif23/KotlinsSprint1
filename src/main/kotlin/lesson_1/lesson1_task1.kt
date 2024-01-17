0cc0ac46bfc7a415d8264b5a38fdf055807c47dc
olegs <olegsavinykh@yahoo.com>
2024-01-17 23:07:10 +0300

    KS-1-1-1 solving the first task

---
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_1
+
+fun main () {
+
+    val aBottleOfWater : Int = 30
+    val textWithThanks : String =  "Мы очень рады,что вы воспользовались услугами " +
+            "нашего магазина! "
+
+    println(textWithThanks)
+}
